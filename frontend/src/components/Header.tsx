be1fbf52f3f5ecf33219dd099f5d518d70191462
Joar Grimstvedt <36162949+joargr@users.noreply.github.com>
2025-08-22 15:36:42 +0200

    Check for and show project (#50)
    * ENH: Introduce TanStack Router
    
    * ENH: Introduce TanStack Query
    
    * ENH: Introduce Hey API openapi-ts
    
    * DOC: Add documentation on formatting and linting
    
    * DOC: Small update on formatting and linting
    
    * ENH: Get API token from URL fragment
    
    * ENH: Define default handling of not found and errors
    
    * ENH: Introduce EDS and styled-components, and add header component
    
    * ENH: Add sidebar and some preliminary routes
    
    * ENH: Use a root context, and store current directory there
    
    * MAINT: Update dependencies
    
    * ENH: Refactoring of API token/not-found/directory checks
    
    * ENH: Handle API token and current project directory
    
    * ENH: Handle unauthorized-error from API
    
    * DEV: Disable non-TypeScript aware rule in linter
    
    * ENH: Add service for getting status and data for active project
    
    * ENH: Add project menu subitems only when active project
    
    * ENH: Show active project in header
    
    * MAINT: Remove obsolete enum
    
    * ENH: Some style adjustments
    
    * ENH: Don't show project masterdata page when no project is set

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,22 @@ import { Button, TopBar, Typography } from "@equinor/eds-core-react";
 import { Link } from "@tanstack/react-router";
 
 import fmuLogo from "../assets/fmu_logo.png";
-import { FmuLogo, HeaderContainer } from "./Header.style";
+import { useProject } from "../services/project";
+import { FmuLogo, HeaderContainer, ProjectInfoContainer } from "./Header.style";
+
+function ProjectInfo() {
+  const { data: project } = useProject();
+  return (
+    <ProjectInfoContainer>
+      Project:{" "}
+      {project.status && project.data ? (
+        <span>{project.data.project_dir_name}</span>
+      ) : (
+        "(not set)"
+      )}
+    </ProjectInfoContainer>
+  );
+}
 
 export function Header() {
   return (
@@ -19,6 +34,9 @@ export function Header() {
           </Button>
           <Typography variant="h1_bold">FMU Settings</Typography>
         </TopBar.Header>
+        <TopBar.Actions>
+          <ProjectInfo />
+        </TopBar.Actions>
       </TopBar>
     </HeaderContainer>
   );
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -2,7 +2,21 @@ import { SideBar as EdsSideBar } from "@equinor/eds-core-react";
 import { account_circle, dashboard, folder } from "@equinor/eds-icons";
 import { Link } from "@tanstack/react-router";
 
+import { useProject } from "../services/project";
+
+type AccordianSubItem = {
+  label: string;
+  to: string;
+};
+
 export function Sidebar() {
+  const { data: project } = useProject();
+
+  const ProjectSubItems: AccordianSubItem[] = [];
+  if (project.status) {
+    ProjectSubItems.push({ label: "Masterdata", to: "/project/masterdata" });
+  }
+
   return (
     <EdsSideBar open>
       <EdsSideBar.Content>
@@ -20,11 +34,14 @@ export function Sidebar() {
             as={Link}
             to="/project/overview"
           />
-          <EdsSideBar.AccordionItem
-            label="Masterdata"
-            as={Link}
-            to="/project/masterdata"
-          />
+          {ProjectSubItems.map((item) => (
+            <EdsSideBar.AccordionItem
+              key={item.to}
+              label={item.label}
+              as={Link}
+              to={item.to}
+            />
+          ))}
         </EdsSideBar.Accordion>
       </EdsSideBar.Content>
     </EdsSideBar>
