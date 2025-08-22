7f948da1f7e825c5a1a7ebe228e4777a4686b544
Joar Grimstvedt <joarg@equinor.com>
2025-08-22 11:46:05 +0200

    ENH: Show active project in header

--- frontend/src/components/Header.style.ts
+++ frontend/src/components/Header.style.ts
@@ -1,3 +1,4 @@
+import { tokens } from "@equinor/eds-tokens";
 import styled from "styled-components";
 
 export const HeaderContainer = styled.div``;
@@ -5,4 +6,11 @@ export const HeaderContainer = styled.div``;
 export const FmuLogo = styled.img`
   width: 35px;
   height: auto;
+`;
+
+export const ProjectInfoContainer = styled.div`
+  padding: 0.5em;
+  border: solid 1px ${tokens.colors.text.static_icons__secondary.hex};
+  background: ${tokens.colors.ui.background__medium.hex};
+  color: ${tokens.colors.text.static_icons__secondary.hex};
 `;
