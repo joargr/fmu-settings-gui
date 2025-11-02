9b882c63f55ae053be9f7953d2b9044cd31e1d33
nourinmohd <nourin_mohd97@outlook.com>
2025-11-02 19:55:37 +0100

    ENH: Add links to homepage

--- frontend/src/routes/index.style.ts
+++ frontend/src/routes/index.style.ts
@@ -1,3 +1,5 @@
+import { Card } from "@equinor/eds-core-react";
+import { tokens } from "@equinor/eds-tokens";
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
@@ -21,7 +23,35 @@ export const AppContainer = styled.div`
   .content {
     grid-area: content;
     overflow: scroll;
-    max-width: 45em;
+    max-width: 55em;
     padding: 24px;
   }
+`;
+
+export const StyledContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  gap: ${tokens.spacings.comfortable.medium};
+`;
+
+export const CardContainer = styled(Card)`
+  width: 250px; 
+  border: solid 1px ${tokens.colors.ui.background__medium.hex};
+  background: ${tokens.colors.ui.background__light.hex};  
+`;
+
+export const SumoLogo = styled.img`
+  width: 35px;
+  height: auto;
+`;
+
+export const WebvizLogo = styled.img`
+  width: 35px;
+  height: auto;
+`;
+
+export const ErtLogo = styled.img`
+  width: 35px;
+  height: auto;
 `;
