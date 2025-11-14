97989e0a3057f56fa83df92038c293680143a54e
Therese Natterøy <61694854+tnatt@users.noreply.github.com>
2025-11-14 13:10:10 +0100

    MAINT: Remove unused code from index.style.ts (#131)

--- frontend/src/routes/index.style.ts
+++ frontend/src/routes/index.style.ts
@@ -1,5 +1,3 @@
-import { Card } from "@equinor/eds-core-react";
-import { tokens } from "@equinor/eds-tokens";
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
@@ -26,32 +24,4 @@ export const AppContainer = styled.div`
     max-width: 55em;
     padding: 24px;
   }
-`;
-
-export const StyledContainer = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  flex-wrap: wrap;
-  gap: ${tokens.spacings.comfortable.medium};
-`;
-
-export const CardContainer = styled(Card)`
-  width: 250px; 
-  border: solid 1px ${tokens.colors.ui.background__medium.hex};
-  background: ${tokens.colors.ui.background__light.hex};  
-`;
-
-export const SumoLogo = styled.img`
-  width: 35px;
-  height: auto;
-`;
-
-export const WebvizLogo = styled.img`
-  width: 35px;
-  height: auto;
-`;
-
-export const ErtLogo = styled.img`
-  width: 35px;
-  height: auto;
 `;
